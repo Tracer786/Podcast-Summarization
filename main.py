4636cc3467739b11fcc0f14997fc82f82a580f9f
Tracer786 <msingh6_be20@thapar.edu>
2024-03-16 11:24:12 +0530

    Completed Streamlit Application
    We have defined all the parameters and the frontend for the streamlit application. We can upload any episode ID from the listen notes API and get the podcast summary.

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 from api_communication import *
 import streamlit as st
+import json
 
-st.title('Welcome to my application that creates podcast summaries.')
-st.sidebar.text_input('Please input an episode id')
-st.sidebar.button('Get podcast summary!')
+st.title('Podcast Summarization')
+episode_id = st.sidebar.text_input('Episode ID')
+button = st.sidebar.button('Get Summary!',on_click=save_transcript, args=(episode_id,))
+
+if button:
+    filename =episode_id + '_chapters.json'
+    with open(filename,'r') as f:
+        data = json.load(f)
+        
+        chapters = data['chapters']
+        podcast_title = data['podcast_title']
+        episode_title = data['episode_title']
+        thumbnail = data['episode_thumbnail']
+        
+    st.header(f'{podcast_title} - {episode_title}')
+    st.image(thumbnail)
+
+    for chp in chapters:
+        with st.expander(chp['gist']):
+            chp['summary']
 
 # save_transcript('3638f68447ea4b75a937a661a60c3f5f')
