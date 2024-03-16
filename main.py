821d1546805a1be7298db9821ff9ef7837983745
Tracer786 <msingh6_be20@thapar.edu>
2024-03-16 10:59:30 +0530

    Not call the function in file
    Will not call the save_transcript function again and again to save the time

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ st.title('Welcome to my application that creates podcast summaries.')
 st.sidebar.text_input('Please input an episode id')
 st.sidebar.button('Get podcast summary!')
 
-save_transcript('3638f68447ea4b75a937a661a60c3f5f')
+# save_transcript('3638f68447ea4b75a937a661a60c3f5f')
