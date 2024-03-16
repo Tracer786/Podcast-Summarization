57b235518aedf2647ccb100f8f5d643c56fc4b51
Tracer786 <msingh6_be20@thapar.edu>
2024-03-16 10:57:05 +0530

    Bringing up the buttons
    Now will start building the flow for our streamlit application.Will give the buttons to the page to upload the episode id and get the summary of the episode

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from api_communication import *
 import streamlit as st
 
-st.title('This is a title')
+st.title('Welcome to my application that creates podcast summaries.')
+st.sidebar.text_input('Please input an episode id')
+st.sidebar.button('Get podcast summary!')
 
 save_transcript('3638f68447ea4b75a937a661a60c3f5f')
