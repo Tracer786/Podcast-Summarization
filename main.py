3462b68919e5bac78c84e31cc9e1e3c690d92259
Tracer786 <msingh6_be20@thapar.edu>
2024-03-16 11:30:37 +0530

    Display chapter time
    As we are using Assembly AI to perform the chapterization, we will also provide the time stamp for each chapter.

--- main.py
+++ main.py
@@ -6,6 +6,17 @@ st.title('Podcast Summarization')
 episode_id = st.sidebar.text_input('Episode ID')
 button = st.sidebar.button('Get Summary!',on_click=save_transcript, args=(episode_id,))
 
+def get_clean_time(start_ms):
+    seconds = int((start_ms / 1000) % 60)
+    minutes = int((start_ms / (1000 * 60)) % 60)
+    hours = int((start_ms / (1000 * 60 * 60)) % 24)
+    if hours > 0:
+        start_t = f'{hours:02d}:{minutes:02d}:{seconds:02d}'
+    else:
+        start_t = f'{minutes:02d}:{seconds:02d}'
+        
+    return start_t
+
 if button:
     filename =episode_id + '_chapters.json'
     with open(filename,'r') as f:
@@ -20,7 +31,7 @@ if button:
     st.image(thumbnail)
 
     for chp in chapters:
-        with st.expander(chp['gist']):
+        with st.expander(chp['gist'] + '-' + get_clean_time(chp['start'])):
             chp['summary']
 
 # save_transcript('3638f68447ea4b75a937a661a60c3f5f')
